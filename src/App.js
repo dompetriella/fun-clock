d819f023b07bddf5b6c7bc5102ad65b844298c90
Dominic Petriella <50121548+dompetriella@users.noreply.github.com>
2022-03-02 19:58:20 -0500

    started project and set up css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import DigClock from './components/DigClock'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <main>
+            <section>
+            <DigClock 
+            />
+            </section>
+        </main>
+    );
 }
 
 export default App;
